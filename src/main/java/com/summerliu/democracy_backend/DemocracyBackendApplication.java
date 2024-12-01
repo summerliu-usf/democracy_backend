503e77b0289b7f08b9652fde4df1f66ab5a56268
summerliu-usf <kliu49@usfca.edu>
2024-12-01 10:40:08 -0800

    cannot run locally

---
+++ src/main/java/com/summerliu/democracy_backend/DemocracyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.summerliu.democracy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocracyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemocracyBackendApplication.class, args);
+    }
+}
